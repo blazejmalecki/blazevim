1f5241f003f01a80dd177b0cea83e0a9b485574b
Blazej Malecki <malecki.bla@gmail.com>
2025-06-09 00:19:54 +0200

    Add icons

---
+++ lua/plugins/icons.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    {
+      "echasnovski/mini.icons",
+      version = false,
+      opts = {},
+    },
+    {
+      "nvim-tree/nvim-web-devicons",
+      opts = {},
+    }
+  }
+}
