f314573b8ff53e31d42171cb8b081770bc5d11d5
Blazej Malecki <malecki.bla@gmail.com>
2025-06-10 00:17:29 +0200

    Add keymaps descriptions

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,4 +2,13 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {},
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = true })
+      end,
+      desc = "[WhichKey] Show keymaps"
+    },
+  },
 }
