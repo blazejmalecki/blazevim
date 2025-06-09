96f0649fe5747d753a036e1b4561b10120b6ac0d
Blazej Malecki <malecki.bla@gmail.com>
2025-06-09 17:16:17 +0200

    Combine colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    init = function()
+      vim.cmd [[colorscheme catppuccin-macchiato]]
+    end,
+    opts = {},
+  },
+}
