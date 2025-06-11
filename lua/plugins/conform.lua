004287ebefa9cbde27e7bb1ff47fb0996b2498b4
Blazej Malecki <malecki.bla@gmail.com>
2025-06-11 02:07:10 +0200

    Add Conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true })
+      end,
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = {
+      timeout_ms = 500,
+    },
+  },
+}
